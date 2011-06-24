489932a4218afed4177cbe1e8a742b045f6acf7d
Lorin Beer <lorin.beer@nitobi.com>
2011-06-24 11:49:30 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//================================================================================================================
+
+//================================================================================================================
+function populateList() {
+	$("#contentlist").empty(); //empty the list elements we've placed in the html
+	//repopulate the list with data from phonegap's device api
+	$("#contentlist").append("<li><a href='#'>Device Name: "     +device.name+     "</a></li>");
+	$("#contentlist").append("<li><a href='#'>Device Phonegap: " +device.phonegap+ "</a></li>");
+	$("#contentlist").append("<li><a href='#'>Device Platform: " +device.platform+ "</a></li>");
+	$("#contentlist").append("<li><a href='#'>Device UUID: "     +device.uuid+     "</a></li>");
+	$("#contentlist").append("<li><a href='#'>Device Version: "  +device.version+  "</a></li>");
+	
+	$("#contentlist").listview('refresh'); //must be called if you want the new list elements properly styled
+}
+//================================================================================================================
+function beepbeep() {
+  //  navigator.notification.beep();
+
+    $("#mainpage").page();
+	console.log( "did it work" );
+	alert('You totally just pressed a button!!!!111!');
+	return true;
+}
+//================================================================================================================
